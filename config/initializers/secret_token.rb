f54e8a9c7ad3d181ca593fba5ac0a79d160a3a89
Weera Wu <weera@oozou.com>
2014-02-21 00:28:35 +0700

    Integrate with a new Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PoolTournament::Application.config.secret_key_base = '469e4833ddeda5a076cbc3857ae5dbee90154f0d85a13e20359754c01ef4a0bfe4b0c47d7690d7a4ea272103791810ce6582a06ff30cb1c1eca32290af0fdf5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PoolTournament::Application.config.session_store :cookie_store, key: '_pool-tournament_session'
